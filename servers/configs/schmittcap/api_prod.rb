e4f164eca3c17ac377ed5bceef60ebc3b0c17278
Dat Do <datdo@rjc.co.jp>
2017-12-26 12:35:56 +0900

    Add configs for furumoto, redhorse, schmittcap, winwin-pro

---
+++ servers/configs/schmittcap/api_prod.rb
@@ -0,0 +1,31 @@
+{
+  active: true,
+  name: 'schmittcap/api/prod',
+  url: 'https://schmittcap.wealth-park.com/api/v4/check_alive',
+  interval: 1,
+  alert: ['butterfly.dev@rjc.co.jp'],
+=begin
+  ssh: {
+    user: 'ubuntu',
+    server: 'api.wealth-park.com',
+    has_sudo_priviledge: true
+  },
+  log: {
+    path: [
+      '/home/ubuntu/wealthpark-api/server/log/production.log',
+      '/home/ubuntu/wealthpark-api/server/rpc/log/forever.err',
+      '/home/ubuntu/wealthpark-api/server/rpc/log/forever.log',
+      '/home/ubuntu/wealthpark-api/server/rpc/log/forever.out',
+      '/var/log/nginx/error.log',
+      '/var/log/nginx/access.log'
+    ],
+    lines: 2000
+  },
+  start_script: "
+    cd /home/ubuntu/wealthpark-api/server;
+    scripts/deploy/api.sh restart production;
+    scripts/deploy/rpc.sh restart production;
+    sudo service nginx restart;
+  "
+=end
+}
