db0a0d51906b0e5d369786a06cb03e2bd4698214
ubuntu <ubuntu@ip-172-31-4-68.us-west-2.compute.internal>
2018-08-24 17:15:02 +0900

    Add new pm

---
+++ servers/configs/gateway/services/lattrait/api.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/lattrait/api',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/lattrait-api',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
---
+++ servers/configs/gateway/services/lattrait/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/lattrait/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/lattrait-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
