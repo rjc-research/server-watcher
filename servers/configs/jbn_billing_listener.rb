1ec493b07ce00b91e84a1a2b30444c63e0b38497
Dat Do <datdo@rjc.co.jp>
2018-09-09 20:03:17 +0900

    Comment out start script for billing and rjc

--- servers/configs/jbn_billing_listener.rb
+++ servers/configs/jbn_billing_listener.rb
@@ -4,14 +4,14 @@
   url: 'http://prod.wealth-park.com/billing/alive',
   interval: INTERVAL,
   alert: ALERT_EMAIL_ADDRESSES,
-  ssh: {
-    user: 'ubuntu',
-    server: 'prod.wealth-park.com',
-    has_sudo_priviledge: true
-  },
-  log: {
-    path: ['/var/log/apache2/error_wealth-park.log'],
-    lines: 2000
-  },
-  start_script: '/etc/init.d/apache2 restart'
+  # ssh: {
+  #   user: 'ubuntu',
+  #   server: 'prod.wealth-park.com',
+  #   has_sudo_priviledge: true
+  # },
+  # log: {
+  #   path: ['/var/log/apache2/error_wealth-park.log'],
+  #   lines: 2000
+  # },
+  # start_script: '/etc/init.d/apache2 restart'
 }
--- servers/configs/rjcweb.rb
+++ servers/configs/rjcweb.rb
@@ -4,10 +4,10 @@
   url: 'https://www.rjc.co.jp',
   interval: INTERVAL,
   alert: ALERT_EMAIL_ADDRESSES,
-  ssh: {
-    user: 'bitnami',
-    server: 'rjc.co.jp',
-    has_sudo_priviledge: true
-  },
-  start_script: 'sudo /opt/bitnami/ctlscript.sh restart'
+  # ssh: {
+  #   user: 'bitnami',
+  #   server: 'rjc.co.jp',
+  #   has_sudo_priviledge: true
+  # },
+  # start_script: 'sudo /opt/bitnami/ctlscript.sh restart'
 }
