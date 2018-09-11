8b6f1d341851bc40252097e75b7ad4b01a4d8002
Dat Do <datdo@rjc.co.jp>
2018-09-11 19:03:05 +0900

    Remote m0001

--- servers/configs/gateway/services/m0001/storage.rb
+++
@@ -1,7 +0,0 @@
-{
-  active: true,
-  name: 'gateway/services/m0001/storage',
-  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/m0001-storage',
-  interval: INTERVAL,
-  alert: ALERT_EMAIL_ADDRESSES,
-}
