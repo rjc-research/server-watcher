08a8c4527c6fd8ec5b07ca51e9992f9f425011c9
Dat Do <datdo@rjc.co.jp>
2018-09-14 11:25:25 +0900

    Add countly and metabase

---
+++ servers/configs/countly.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'countly',
+  url: 'https://countly.wealth-park.com',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
---
+++ servers/configs/metabase.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'metabase',
+  url: 'http://metabase.wealth-park.com',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
