4e74a626eca5363c16f3e372306ac876fbfd28de
Dat Do <dat@ggcontent.com>
2019-10-11 03:00:47 +0900

    Fix "hothouse-chat" => "hothouse"

--- servers/configs/hothouse/chat_http_prod.rb
+++ servers/configs/hothouse/chat_http_prod.rb
@@ -1,8 +1,7 @@
 {
   active: true,
   name: 'hothouse/chat/prod/http',
-  #url: 'https://hothouse.wealth-park.com/chat/api/check_alive',
-  url: 'https://hothouse-chat.wealth-park.com/api/check_alive',
+  url: 'https://hothouse.wealth-park.com/chat/api/check_alive',
   interval: INTERVAL,
   alert: ALERT_EMAIL_ADDRESSES,
   custom_http_check: CHAT_CUSTOM_HTTP_CHECK,
