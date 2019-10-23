5d08d33c834cbc8e3cc5ab0e998c02a94709721d
Dat Do <datdo@wealth-park.com>
2019-10-23 17:12:25 +0900

    Add new PM: koryo-j

---
+++ servers/configs/gateway/services/koryo-j/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/koryo-j/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/koryo-j-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
---
+++ servers/configs/koryo-j/potato_prod.rb
@@ -0,0 +1,10 @@
+{
+  active: true,
+  name: 'koryo-j/potato/production',
+  url: [
+    'https://koryo-j.wealth-park.com',
+  ],
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: POTATO_CUSTOM_HTTP_CHECK,
+}
