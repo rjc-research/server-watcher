e09369f245f77ca36286a945b88d8b0a3543d4dd
Dat Do <datdo@wealth-park.com>
2019-10-30 18:05:38 +0900

    Add new PM: ryowahouse

---
+++ servers/configs/ryowahouse/storage_prod.rb
@@ -0,0 +1,8 @@
+{
+  active: true,
+  name: 'ryowahouse/storage/prod',
+  url: 'https://ryowahouse.wealth-park.com/storage/api/v1/check_alive',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: STORAGE_CUSTOM_HTTP_CHECK,
+}
