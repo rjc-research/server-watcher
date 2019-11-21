006868571d24b7336c17ba36c40025e91ee79ea5
Dat Do <datdo@wealth-park.com>
2019-11-22 02:30:11 +0900

    Add new PM: trust-realestate

---
+++ servers/configs/gateway/services/trust-realestate/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/trust-realestate/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/trust-realestate-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
