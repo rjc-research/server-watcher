a8245c7aec305a0e686fd04df06b93fb255fe662
Dat Do <datdo@wealth-park.com>
2019-12-11 15:46:16 +0900

    Add new PM: starmica

---
+++ servers/configs/gateway/services/starmica/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/starmica/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/starmica-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
---
+++ servers/configs/starmica/potato_prod.rb
@@ -0,0 +1,10 @@
+{
+  active: true,
+  name: 'starmica/potato/production',
+  url: [
+    'https://starmica.wealth-park.com',
+  ],
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: POTATO_CUSTOM_HTTP_CHECK,
+}
