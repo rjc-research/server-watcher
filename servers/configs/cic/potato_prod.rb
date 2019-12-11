ad05f74ab4606dfef930e8f89d92bf29eb592059
Dat Do <datdo@wealth-park.com>
2019-12-11 15:39:09 +0900

    Add new PM: cic

---
+++ servers/configs/cic/potato_prod.rb
@@ -0,0 +1,10 @@
+{
+  active: true,
+  name: 'cic/potato/production',
+  url: [
+    'https://cic.wealth-park.com',
+  ],
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: POTATO_CUSTOM_HTTP_CHECK,
+}
