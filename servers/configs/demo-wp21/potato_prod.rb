eca7510607f8da6f4d3f4be4613f317f0073a7a3
Dat Do <datdo@wealth-park.com>
2019-12-20 15:49:04 +0900

    Add new PM: demo-wp21

---
+++ servers/configs/demo-wp21/potato_prod.rb
@@ -0,0 +1,10 @@
+{
+  active: true,
+  name: 'demo-wp21/potato/production',
+  url: [
+    'https://demo-wp21.wealth-park.com',
+  ],
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: POTATO_CUSTOM_HTTP_CHECK,
+}
---
+++ servers/configs/demo-wp21/storage_prod.rb
@@ -0,0 +1,8 @@
+{
+  active: true,
+  name: 'demo-wp21/storage/prod',
+  url: 'https://demo-wp21.wealth-park.com/storage/api/v1/check_alive',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: STORAGE_CUSTOM_HTTP_CHECK,
+}
