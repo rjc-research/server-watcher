d0c1a67b8de827b95ecd52bc6e51cb4403122ee1
Dat Do <datdo@wealth-park.com>
2020-01-06 15:01:11 +0900

    Add new PM: makes

---
+++ servers/configs/gateway/services/makes/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/makes/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/makes-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
---
+++ servers/configs/makes/potato_prod.rb
@@ -0,0 +1,10 @@
+{
+  active: true,
+  name: 'makes/potato/production',
+  url: [
+    'https://makes.wealth-park.com',
+  ],
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: POTATO_CUSTOM_HTTP_CHECK,
+}
