c1748d5d3d49f8e6292215cbf6309332c4e9cc77
Dat Do <datdo@wealth-park.com>
2020-01-08 21:35:33 +0900

    Add new PM: peter

---
+++ servers/configs/gateway/services/peter/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/peter/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/peter-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
