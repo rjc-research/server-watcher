ed2f40aa0b9304398cdb7bb40c204eba7123da16
Dat Do <datdo@wealth-park.com>
2020-01-10 11:47:58 +0900

    Add new PM: daimlar

---
+++ servers/configs/daimlar/potato_prod.rb
@@ -0,0 +1,10 @@
+{
+  active: true,
+  name: 'daimlar/potato/production',
+  url: [
+    'https://daimlar.wealth-park.com',
+  ],
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: POTATO_CUSTOM_HTTP_CHECK,
+}
---
+++ servers/configs/gateway/services/daimlar/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/daimlar/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/daimlar-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
