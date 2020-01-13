0d9a70d68324f6fc3ee5b7ff796978d384b75949
Dat Do <datdo@wealth-park.com>
2020-01-13 12:54:42 +0900

    Add new PM: petertest

---
+++ servers/configs/gateway/services/petertest/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/petertest/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/petertest-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
---
+++ servers/configs/petertest/storage_prod.rb
@@ -0,0 +1,8 @@
+{
+  active: true,
+  name: 'petertest/storage/prod',
+  url: 'https://petertest.wealth-park.com/storage/api/v1/check_alive',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: STORAGE_CUSTOM_HTTP_CHECK,
+}
