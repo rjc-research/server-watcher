727011c29c9541c09462cc437dd80aff775e0e40
Dat Do <datdo@wealth-park.com>
2020-01-20 11:43:08 +0900

    Add new PM: sales-demo

---
+++ servers/configs/gateway/services/sales-demo/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/sales-demo/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/sales-demo-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
