19f1a967a6cac7dbf6b0d50cc003956cace6f10b
Dat Do <datdo@wealth-park.com>
2020-01-21 13:28:54 +0900

    Add new PM: demo-wp24

---
+++ servers/configs/gateway/services/demo-wp24/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/demo-wp24/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/demo-wp24-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
