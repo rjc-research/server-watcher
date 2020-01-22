05c4335643d678e5c30365bc92f1162d1132ef75
Dat Do <datdo@wealth-park.com>
2020-01-22 16:47:42 +0900

    Add new PM: demo-wp30

---
+++ servers/configs/demo-wp30/potato_prod.rb
@@ -0,0 +1,10 @@
+{
+  active: true,
+  name: 'demo-wp30/potato/production',
+  url: [
+    'https://demo-wp30.wealth-park.com',
+  ],
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: POTATO_CUSTOM_HTTP_CHECK,
+}
---
+++ servers/configs/gateway/services/demo-wp30/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/demo-wp30/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/demo-wp30-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
