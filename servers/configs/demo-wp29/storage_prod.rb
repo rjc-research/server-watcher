37ca90720c91b52eb4fe9a86df66de74858d4dc6
Dat Do <datdo@wealth-park.com>
2020-01-22 16:03:51 +0900

    Add new PM: demo-wp29

---
+++ servers/configs/demo-wp29/storage_prod.rb
@@ -0,0 +1,8 @@
+{
+  active: true,
+  name: 'demo-wp29/storage/prod',
+  url: 'https://demo-wp29.wealth-park.com/storage/api/v1/check_alive',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: STORAGE_CUSTOM_HTTP_CHECK,
+}
