42caba9743d7b38975d0ddbcd45588e9e180707e
Dat Do <datdo@wealth-park.com>
2020-02-26 15:25:40 +0900

    Add new PM: jandk

---
+++ servers/configs/gateway/services/jandk/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/jandk/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/jandk-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
---
+++ servers/configs/jandk/potato_prod.rb
@@ -0,0 +1,10 @@
+{
+  active: true,
+  name: 'jandk/potato/production',
+  url: [
+    'https://jandk.wealth-park.com',
+  ],
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: POTATO_CUSTOM_HTTP_CHECK,
+}
