d0939d5796374651cba81a8c436f72838390272a
Dat Do <datdo@wealth-park.com>
2020-03-30 18:43:42 +0900

    Add new PM: tokyobighouse

---
+++ servers/configs/gateway/services/tokyobighouse/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/tokyobighouse/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/tokyobighouse-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
---
+++ servers/configs/tokyobighouse/storage_prod.rb
@@ -0,0 +1,8 @@
+{
+  active: true,
+  name: 'tokyobighouse/storage/prod',
+  url: 'https://tokyobighouse.wealth-park.com/storage/api/v1/check_alive',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: STORAGE_CUSTOM_HTTP_CHECK,
+}
