191634c9ef238342baaa02b8838e72b0cc4c7d22
Dat Do <datdo@wealth-park.com>
2020-03-31 14:11:22 +0900

    Add new PM: misawahome

---
+++ servers/configs/gateway/services/misawahome/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/misawahome/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/misawahome-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
---
+++ servers/configs/misawahome/potato_prod.rb
@@ -0,0 +1,10 @@
+{
+  active: true,
+  name: 'misawahome/potato/production',
+  url: [
+    'https://misawahome.wealth-park.com',
+  ],
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: POTATO_CUSTOM_HTTP_CHECK,
+}
