84b83aaaaddd9b3ff587eb9fb5a99c4d513da63f
Dat Do <datdo@wealth-park.com>
2020-04-15 11:07:32 +0900

    Add new PM: demo-wp32

---
+++ servers/configs/demo-wp32/potato_prod.rb
@@ -0,0 +1,10 @@
+{
+  active: true,
+  name: 'demo-wp32/potato/production',
+  url: [
+    'https://demo-wp32.wealth-park.com',
+  ],
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: POTATO_CUSTOM_HTTP_CHECK,
+}
