0f403a6951ff1e6085a7a72cac5436e5e9b93b1d
Dat Do <datdo@wealth-park.com>
2020-04-17 19:23:29 +0900

    Add new PM: creatas

---
+++ servers/configs/creatas/storage_prod.rb
@@ -0,0 +1,8 @@
+{
+  active: true,
+  name: 'creatas/storage/prod',
+  url: 'https://creatas.wealth-park.com/storage/api/v1/check_alive',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: STORAGE_CUSTOM_HTTP_CHECK,
+}
---
+++ servers/configs/gateway/services/creatas/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/creatas/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/creatas-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
