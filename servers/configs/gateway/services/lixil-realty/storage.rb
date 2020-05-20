91935f7de6dd9df866ceeba7d49982b45853ff98
Dat Do <datdo@wealth-park.com>
2020-05-20 17:05:54 +0900

    Add new PM: lixil-realty

---
+++ servers/configs/gateway/services/lixil-realty/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/lixil-realty/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/lixil-realty-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
