c7c5c2d9da56dfce6b3e9cb1308a6ec7fb92a829
Dat Do <datdo@wealth-park.com>
2020-06-19 08:07:55 +0900

    Add new PM: asahisogo

---
+++ servers/configs/asahisogo/storage_prod.rb
@@ -0,0 +1,8 @@
+{
+  active: true,
+  name: 'asahisogo/storage/prod',
+  url: 'https://asahisogo.wealth-park.com/storage/api/v1/check_alive',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: STORAGE_CUSTOM_HTTP_CHECK,
+}
---
+++ servers/configs/gateway/services/asahisogo/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/asahisogo/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/asahisogo-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
