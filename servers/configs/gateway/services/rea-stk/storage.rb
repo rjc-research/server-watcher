4942d697045cf0968c41bbc7743e635f4a438977
Dat Do <datdo@wealth-park.com>
2020-06-19 08:07:27 +0900

    Add new PM: rea-stk

---
+++ servers/configs/gateway/services/rea-stk/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/rea-stk/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/rea-stk-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
---
+++ servers/configs/rea-stk/storage_prod.rb
@@ -0,0 +1,8 @@
+{
+  active: true,
+  name: 'rea-stk/storage/prod',
+  url: 'https://rea-stk.wealth-park.com/storage/api/v1/check_alive',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: STORAGE_CUSTOM_HTTP_CHECK,
+}
