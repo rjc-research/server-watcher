db8c382e40ec63e5bc5bf909409d483f7c638a05
Dat Do <datdo@wealth-park.com>
2020-06-19 08:07:06 +0900

    Add new PM: worldestate

---
+++ servers/configs/gateway/services/worldestate/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/worldestate/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/worldestate-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
---
+++ servers/configs/worldestate/storage_prod.rb
@@ -0,0 +1,8 @@
+{
+  active: true,
+  name: 'worldestate/storage/prod',
+  url: 'https://worldestate.wealth-park.com/storage/api/v1/check_alive',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: STORAGE_CUSTOM_HTTP_CHECK,
+}
