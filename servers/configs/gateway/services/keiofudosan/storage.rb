f598c3fce5683bbde755ee569a42e03056fcd404
Dat Do <datdo@wealth-park.com>
2020-06-19 08:06:46 +0900

    Add new PM: keiofudosan

---
+++ servers/configs/gateway/services/keiofudosan/storage.rb
@@ -0,0 +1,7 @@
+{
+  active: true,
+  name: 'gateway/services/keiofudosan/storage',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/keiofudosan-storage',
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+}
---
+++ servers/configs/keiofudosan/potato_prod.rb
@@ -0,0 +1,10 @@
+{
+  active: true,
+  name: 'keiofudosan/potato/production',
+  url: [
+    'https://keiofudosan.wealth-park.com',
+  ],
+  interval: INTERVAL,
+  alert: ALERT_EMAIL_ADDRESSES,
+  custom_http_check: POTATO_CUSTOM_HTTP_CHECK,
+}
