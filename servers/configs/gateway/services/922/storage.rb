8cc3925e6401a9a7ca10998645c558e1ffcd0077
Server watcher <ubuntu@sw.wealth-park.com>
2020-10-07 11:04:26 +0900

    Remove PM: 922

--- servers/configs/gateway/services/922/storage.rb
+++
@@ -1,7 +0,0 @@
-{
-  active: true,
-  name: 'gateway/services/922/storage',
-  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/922-storage',
-  interval: INTERVAL,
-  alert: ALERT_EMAIL_ADDRESSES,
-}
