f8103a558ff49b8b9f56444111b976e018654509
Server watcher <ubuntu@sw.wealth-park.com>
2021-03-19 14:36:47 +0900

    Update config for gateway/prod/api/rpc

--- servers/configs/gateway/gateway_prod_api_rpc.rb
+++ servers/configs/gateway/gateway_prod_api_rpc.rb
@@ -1,7 +1,7 @@
 {
   active: true,
   name: 'gateway/prod/api/rpc',
-  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/saas-api',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/bigpotatoapi-prod',
   interval: INTERVAL,
   alert: ALERT_EMAIL_ADDRESSES,
 }
