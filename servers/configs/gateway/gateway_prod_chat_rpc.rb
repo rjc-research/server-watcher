33c2f672badf262e7dc77f96998b3c817f703d85
Dat Do <dat@ggcontent.com>
2021-03-19 09:55:02 +0900

    Update config for "gateway/prod/chat/rpc"

--- servers/configs/gateway/gateway_prod_chat_rpc.rb
+++ servers/configs/gateway/gateway_prod_chat_rpc.rb
@@ -1,7 +1,7 @@
 {
   active: true,
   name: 'gateway/prod/chat/rpc',
-  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/saas-chat',
+  url: 'https://gateway.wealth-park.com/gateway/api/check_service_connecting/bigchat-prod',
   interval: INTERVAL,
   alert: ALERT_EMAIL_ADDRESSES,
 }
